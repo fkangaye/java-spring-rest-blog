eafb135e61f484b24cde0c97012f6570ac9d3b76
Fadila kangaye <fkangaye@yahoo.com>
2022-07-17 22:23:48 -0500

    module 2 done

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -1,14 +1,12 @@
 package com.pluralsight.blog.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.Hibernate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Entity
 public class Author {
@@ -18,11 +16,17 @@ public class Author {
     private Long id;
     private String firstname;
     private String lastname;
+    @JsonIgnore
     private String username;
+    @JsonIgnore
     private String password;
+
+    @OneToMany
+    private List<Post> posts;
 
     public Author() {
         super();
+        posts = new ArrayList<>();
     }
 
     public Author(String username, String firstname, String lastname, String password) {
@@ -34,7 +38,7 @@ public class Author {
     }
 
     public void setPassword(String password) {
-         this.password = PASSWORD_ENCODER.encode(password);
+        this.password = PASSWORD_ENCODER.encode(password);
     }
 
     public Long getId() {
@@ -71,24 +75,28 @@ public class Author {
 
     @Override
     public boolean equals(Object obj) {
-        Author inputAuthor = (Author)obj;
+        Author inputAuthor = (Author) obj;
         if (!this.firstname.equals(inputAuthor.getFirstName())) {
             System.out.println("firstname not equal");
-            return false;}
+            return false;
+        }
         if (!this.lastname.equals(inputAuthor.getLastname())) {
             System.out.println("lastname not equal");
-            return false;}
+            return false;
+        }
         if (!this.username.equals(inputAuthor.getUsername())) {
             System.out.println("username not equal");
-            return false;}
+            return false;
+        }
         return true;
     }
 
     public List<Post> getPosts() {
-        return null;
+        return posts;
     }
 
     public void addPost(Post post) {
-        return;
+        post.setAuthor(this);
+        posts.add(post);
     }
 }
--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -3,14 +3,9 @@ package com.pluralsight.blog.model;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 public class Post {
@@ -20,18 +15,29 @@ public class Post {
 
     private String title;
 
-    @Column(length=1000000)
+    @Column(length = 1000000)
     @Lob
     private String body;
     @Temporal(TemporalType.DATE)
     @CreationTimestamp
     private Date date;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Author author;
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
     public Post() {
         super();
     }
 
-    public Post(String title, String body){//, Author author) {
+    public Post(String title, String body) {
         this();
         this.title = title;
         this.body = body;
@@ -78,8 +84,8 @@ public class Post {
     public boolean equals(Object obj) {
         if (!(obj instanceof Post))
             return false;
-        Post otherPost = (Post)obj;
+        Post otherPost = (Post) obj;
         return this.title.equals(otherPost.getTitle()) &&
-               this.body.equals(otherPost.getBody());
+                this.body.equals(otherPost.getBody());
     }
 }
